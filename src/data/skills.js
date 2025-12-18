1f00638026641aa974acd99504c8b1e9258e00c7
Dhyey Patel <dhyeypatel1205@gmail.com>
2025-12-18 13:48:13 -0600

    Delete portfolio website project files.

--- src/data/skills.js
+++
@@ -1,6 +0,0 @@
-export const skills = {
-    languages: ["Python", "SQL"],
-    ml_dl: ["TensorFlow", "Scikit-learn", "PyTorch", "Transformers", "LLMs", "RAG", "Deep Learning", "Machine Learning"],
-    tools: ["Git", "Tableau", "Docker", "AWS", "MLFLOW", "Streamlit", "Excel", "Visual Studio"],
-    concepts: ["NLP", "Computer Vision", "Data Mining", "EDA", "AI Agents", "AI Workflow", "Model Evaluation"]
-};
